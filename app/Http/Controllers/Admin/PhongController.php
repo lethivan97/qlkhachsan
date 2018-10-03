e0166d7d5e7f77805e9b2ea449c1e5981f3a17ef
levan1997 <levan.gtvt.97@gmail.com>
2018-10-03 20:32:19 +0700

    update

--- app/Http/Controllers/Admin/PhongController.php
+++ app/Http/Controllers/Admin/PhongController.php
@@ -2,21 +2,22 @@
 
 namespace App\Http\Controllers\Admin;
 
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Pagination\LengthAwarePaginator;
 
-class PhongController extends Controller
-{
-    public function index(){
-    	//$data = DB::table('Phong')->get();
-    	$data = DB::select('
-    		SELECT MaPhong, TenTT, TenLoai, TenPhong, Phong.MoTa 
-    		FROM Phong, TrangThai, LoaiPhong 
-    		WHERE Phong.MaTT = TrangThai.MaTT 
-    			AND Phong.MaLoai = LoaiPhong.MaLoai
-    		');
-    	return view('admin.phong', ['phongs' => $data]);
-    }
+class PhongController extends Controller {
+	public function index() {
+		//$data = DB::table('Phong')->get();
+		/*$data = DB::select('
+	          SELECT MaPhong, TenTT, TenLoai, TenPhong, Phong.MoTa
+	          FROM Phong, TrangThai, LoaiPhong
+	          WHERE Phong.MaTT = TrangThai.MaTT
+	          AND Phong.MaLoai = LoaiPhong.MaLoai
+*/
+		$data = DB::table('TrangThai')->join('Phong', 'TrangThai.MaTT', '=', 'Phong.MaTT')
+			->join('LoaiPhong', 'Phong.MaLoai', '=', 'LoaiPhong.MaLoai')
+			->select('Phong.MaPhong', 'TenTT', 'TenLoai', 'TenPhong')
+			->paginate(10);
+		return view('admin.phong', ['phongs' => $data]);
+	}
 }
--- resources/views/admin/phong.blade.php
+++ resources/views/admin/phong.blade.php
@@ -1,51 +1,68 @@
 @extends('layouts._share.admin')
 @section('content')
 
-	<div class="container">
-	  <h2>Danh sách phòng</h2>
-
-	  <input type="text" id="search" placeholder="Nhập tên phòng" onkeyup="searchFunction();">
-
-	  <table class="table table-striped" id="tableId">
-	  	<thead>
-	      <tr>
-	        <th>Mã phòng</th>
-	        <th>Trạng thái</th>
-	        <th>Loại phòng</th>
-	        <th>Tên phòng</th>
-	        <th>Mô tả</th>
-	      </tr>
-	    </thead>
-	    <tbody>
-	    	@foreach($phongs as $row)
-	    		<tr>
-		        @foreach($row as $value)
-		        	<td>{{$value}}</td>
-		        @endforeach
-		      </tr>
-	    	@endforeach
-	    </tbody>
-	  </table>
+<div class="container">
+	<h2>Danh sách phòng</h2>
+	<div class="row">
+		<div class="col-md-6">
+			<input type="text" style="width: 300px" id="search" placeholder="Nhập tên phòng" onkeyup="searchFunction();" class="form-control">
+		</div>
+		<div class="col-md-6">
+			<button class="btn btn-info btn-sm" style="float: right;">Thêm mới</button>
+		</div>
 	</div>
 
-	<script type="text/javascript">
-		function searchFunction() {
-		    var input, filter, table, tr, td, i;
-		    input = document.getElementById("search");
-		    filter = input.value.toUpperCase();
-		    table = document.getElementById("tableId");
-		    tr = table.getElementsByTagName("tr");
-		    for (i = 0; i < tr.length; i++) {
-			    td = tr[i].getElementsByTagName("td")[3];
-			    if (td) {
-			        if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
-			            tr[i].style.display = "";
-			        } else {
-			            tr[i].style.display = "none";
-			        }
-			    }       
-		    }
+
+	<table class="table table-striped" id="tableId" style="margin-top: 20px">
+		<thead>
+			<tr>
+				<th>STT</th>
+				<th>Trạng thái</th>
+				<th>Loại phòng</th>
+				<th>Tên phòng</th>
+				<th>Action</th>
+			</tr>
+		</thead>
+		<tbody>
+			@foreach($phongs as $row)
+			<tr>
+				<td>{{$row->MaPhong}}</td>
+				<td>{{$row->TenTT}}</td>
+				<td>{{$row->TenLoai}}</td>
+				<td>{{$row->TenPhong}}</td>
+				<td>
+					<button class="btn btn-sm btn-primary">Sửa</button>
+					<button class="btn btn-sm btn-danger">Xóa</button>
+				</td>
+			</tr>
+			@endforeach
+		</tbody>
+	</table>
+	<div class="row">
+		<div class="col-md-12 text-center">
+			{{$phongs->links()}}
+		</div>
+	</div>
+</div>
+
+<script type="text/javascript">
+	function searchFunction() {
+		var input, filter, table, tr, td, i;
+		input = document.getElementById("search");
+		filter = input.value.toUpperCase();
+		table = document.getElementById("tableId");
+		tr = table.getElementsByTagName("tr");
+		for (i = 0; i < tr.length; i++) {
+			td = tr[i].getElementsByTagName("td")[3];
+			if (td) {
+				if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+					tr[i].style.display = "";
+				} else {
+					tr[i].style.display = "none";
+				}
+			}
 		}
-	</script>
+	}
+</script>
 
 @endsection
