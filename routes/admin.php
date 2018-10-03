3a51142e6b0a4121a9c05a04b1d3321563e34a5a
levan1997 <levan.gtvt.97@gmail.com>
2018-10-03 22:40:50 +0700

    fix

--- routes/admin.php
+++ routes/admin.php
@@ -2,13 +2,7 @@
 Route::get('/', function () {
 	return view('admin');
 })->name('admin');
-<<<<<<< HEAD
 Route::get('phong', 'Admin\PhongController@index')->name('admin.phong');
-=======
-
-Route::get('phong', 'Admin\PhongController@index')->name('adminphong');
-
->>>>>>> a1369c77584218cde95804ee4257411a865f1caa
 ?>
 
 
