e5aa7f904d1f14f734736f14ce97732ad978fc4f
levan1997 <levan.gtvt.97@gmail.com>
2018-11-08 09:01:37 +0700

    update

--- resources/views/layouts/_share/admin/header.blade.php
+++ resources/views/layouts/_share/admin/header.blade.php
@@ -5,7 +5,7 @@
       <a class="nav-link" data-widget="pushmenu" href="#"><i class="fa fa-bars"></i></a>
     </li>
     <li class="nav-item d-none d-sm-inline-block">
-      <a href="index3.html" class="nav-link">Trang chủ</a>
+      <a href="{{route('admin')}}" class="nav-link">Trang chủ</a>
     </li>
     <li class="nav-item d-none d-sm-inline-block">
       <a href="{{ route('logout') }}"
