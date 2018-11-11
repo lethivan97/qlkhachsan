74cfb2d68aec7b0af357b10d04ec1c805769f028
levan1997 <levan.gtvt.97@gmail.com>
2018-11-11 10:54:40 +0700

    change pw

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -4,10 +4,9 @@ namespace App\Http\Controllers\Admin;
 
 use App\DAO\UserDAO;
 use App\Http\Controllers\Controller;
-
 use App\Models\User;
-
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller {
 	public function index() {
@@ -21,35 +20,45 @@ class UserController extends Controller {
 	public function deleteUser(Request $request) {
 		dd($request->all());
 	}
-	
+
 	public function xoaUser($id) {
 		User::where('id', $id)->delete();
 		return redirect()->route('admin.user');
 	}
 	public function chiTietUser($id) {
 		$user = User::where('id', $id)->first();
-		
-		return view('admin.user.sua-user',compact('user'));
+
+		return view('admin.user.sua-user', compact('user'));
 	}
 	public function savechiTietUser($id, Request $request) {
-		$user = new User();
-		$user = User::where('id', $id)->first();
-		if ($user && password_verify($request->password, $user->password)){
-			if($request->new_pass==$request->re_new_pass){
-				$user = new User();
-				$user->password = Hash::make($request->new_pass);
-				User::where("id", $id)->update($user->toArray());
-				return redirect()->route('admin.user');
+		$this->validate($request,
+			[
+				'password' => 'required',
+				'new_password' => 'required|min:6',
+				're_new_password' => 'required|min:6|same:new_password',
+			],
+			[
+				'password.required' => 'Nhập password',
+				'new_password.required' => 'Nhập password mới',
+				'new_password.min' => "Password chứa ít nhất 6 ký tự",
+				'new_password.required' => "Nhập xác nhận Password",
+				're_new_password.min' => "Password chứa ít nhất 6 ký tự",
+				're_new_password.same' => "Password không khớp",
+			]
+		);
+		$user = User::find($id);
+		if ($user && password_verify($request->password, $user->password)) {
+			if ($request->new_password === $request->re_new_password) {
+				User::find($id)->update([
+					'password' => Hash::make($request->new_password),
+				]);
+				return redirect()->back()->with('thongbao', 'Thay đổi password thành công !');
 			}
-			else{
-				return redirect()->back()->with('thongbao','Password mới không trùng nhau');
-			}
+		} else {
+			return redirect()->back()->with('thongbao', 'Password không đúng');
 		}
-		else{
-return redirect()->back()->with('thongbao','Password không đúng');
-		}
+		return redirect()->route('admin.user');
 	}
-
 
 }
 ?>
--- resources/views/admin/user/sua-user.blade.php
+++ resources/views/admin/user/sua-user.blade.php
@@ -5,43 +5,58 @@
 
 <div class="container">
 	<div class="row">
-		<h3 class="text-success" style="margin: 50px 0">Sửa {{$user->name}}</h3>
+		<h3 class="text-success" style="margin: 50px 0">Sửa mật khẩu {{$user->name}}</h3>
 	</div>
 	<div class="row">
-			<div class="col-md-12">
-					@if(Session::has('thongbao'))
-					{{Session::get('thongbao')}}
-					@endif
-				</div>
+		<div class="col-md-12">
+			@if(Session::has('thongbao'))
+			<p class="text-danger">{{Session::get('thongbao')}}</p>
+			@endif
+		</div>
 	</div>
 	<div class="row">
 		<form class="col-md-8" method="POST" id ="formChangePass">
 			@csrf
 			<div class="form-group row">
-				<label for="password" class="col-sm-2 col-form-label">Nhập mật khẩu cũ</label>
-				<div class="col-sm-10" id = "old_pass">
+				<label for="password" class="col-sm-3 col-form-label">Nhập mật khẩu cũ</label>
+				<div class="col-sm-9" id = "old_pass">
 					<input type="password" class="form-control" name="password" placeholder="Nhập password">
 				</div>
-			 </div>
-			 <div class="form-group row">
-				<label for="password" class="col-sm-2 col-form-label">Nhập mật khẩu mới</label>
-				<div class="col-sm-10" id = "new_pass">
-					<input type="password" class="form-control" name="password" placeholder="Nhập password">
+				<div class="col-md-12">
+					@if($errors->has('password'))
+					<p class="text-danger"><i class="fa fa-exclamation-circle"></i> {{$errors->first('password')}}</p>
+					@endif
 				</div>
-			 </div>
-			 <div class="form-group row">
-				<label for="password" class="col-sm-2 col-form-label">Xác nhận mật khẩu mới</label>
-				<div class="col-sm-10" id = "re_new_pass">
-					<input type="password" class="form-control" name="password" placeholder="Nhập password">
+			</div>
+			<div class="form-group row">
+				<label for="password" class="col-sm-3 col-form-label">Nhập mật khẩu mới</label>
+				<div class="col-sm-9" id = "new_pass">
+					<input type="password" class="form-control" name="new_password" placeholder="Nhập password mới">
 				</div>
-			 </div>
-			 <div class="form-group row">
+				<div class="col-md-12">
+					@if($errors->has('new_password'))
+					<p class="text-danger"><i class="fa fa-exclamation-circle"></i> {{$errors->first('new_password')}}</p>
+					@endif
+				</div>
+			</div>
+			<div class="form-group row">
+				<label for="password" class="col-sm-3 col-form-label">Xác nhận mật khẩu mới</label>
+				<div class="col-sm-9" id = "re_new_pass">
+					<input type="password" class="form-control" name="re_new_password" placeholder="Xác nhận password">
+				</div>
+				<div class="col-md-12">
+					@if($errors->has('re_new_password'))
+					<p class="text-danger"><i class="fa fa-exclamation-circle"></i> {{$errors->first('re_new_password')}}</p>
+					@endif
+				</div>
+			</div>
+			<div class="form-group row">
 				<div class="col-md-12  text-center ">
 					<button type="submit" class="btn btn-sm btn-primary">Lưu</button>
 					<a href="{{route('admin.user')}}" class="btn btn-sm btn-default">Thoát</a>
 				</div>
-		</form>
+			</form>
+		</div>
 	</div>
-</div>	
-@endif
-@endsection
+	@endif
+	@endsection
